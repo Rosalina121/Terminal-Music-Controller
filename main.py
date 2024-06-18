bfb2e481ba9be8ae2ce6379335a9855dccd872cf
Alina Rosa <rosalina@palitechnika.com>
2024-06-18 12:24:14 +0200

    change progress bar look, add blending colors for progress timer (WIP)

--- main.py
+++ main.py
@@ -17,22 +17,80 @@ import textwrap
 import subprocess
 import time
 
-screen = curses.initscr()
-screen.immedok(True)
-screen.border(0)
-screen.nodelay(1)
 
-curses.start_color()
-curses.use_default_colors()
-for i in range(0, curses.COLORS):
-    curses.init_pair(i + 1, i, -1)
+def init_color():
+    # curses is truly terminal independent and has r/g/b between 0 and 1000
+    # this is a bit cumbersome if you're used to 0...255
+    # so we need to convert them
+    # r - 0...255
+    # R - 0...1000
+    # Equation:
+    # r = R * .255
+    # R = r / .255
+
+    curses.start_color()
+
+    curses.init_color(1, 0, 0, 0)  # white for progress
+    curses.init_color(2, int(245 / 0.255), int(169 / 0.255), int(184 / 0.255))  # pink
+    curses.init_color(3, int(91 / 0.255), int(206 / 0.255), int(250 / 0.255))  # blue
+
+    curses.init_pair(1, 1, 0)  # white
+    curses.init_pair(2, 2, 0)  # pink
+    curses.init_pair(3, 3, 0)  # blue
+
+    # # set default colors
+    # curses.use_default_colors()
+    # for i in range(0, curses.COLORS):
+    #     curses.init_pair(i + 1, i, -1)
+
 
 def scroll_string(string):
     new_string = string[1:] + string[0]
     return new_string
 
+
 def get_color_from_percentage(base, percentage):
     return base + int(percentage / 20) % 6
+
+
+screen = curses.initscr()
+screen.immedok(True)
+screen.border(0)
+screen.nodelay(1)
+
+init_color()
+
+
+def draw_progress_bar(box, pos_str, length_str, max_x):
+    cur_pos = sum(int(x) * 60**i for i, x in enumerate(pos_str.split(":")[::-1]))
+    cur_length = sum(int(x) * 60**i for i, x in enumerate(length_str.split(":")[::-1]))
+
+    percentage = cur_pos / cur_length * 100
+    percentage = int(percentage)
+    progress_bar = (max_x - 4) * "━"
+    progress_bar = (
+        progress_bar[: int(percentage / 100 * (max_x - 4))]
+        + "○"
+        + progress_bar[int(percentage / 100 * (max_x - 4)) + 1 :]
+    )
+    box.addnstr(6, 2, progress_bar, max_x - 4)
+
+    box.addstr(5, 2, pos_str, curses.color_pair(1))
+
+    box.addstr(5, max_x - 2 - len(length_str), length_str, curses.color_pair(3))
+
+    blend_and_init_color((255, 255, 255), (255, 0, 0), percentage)
+
+
+def blend_and_init_color(base, target, percentage):
+    r_base, g_base, b_base = base
+    r_target, g_target, b_target = target
+
+    r = int(r_base + percentage / 100 * (r_target - r_base))
+    g = int(g_base + percentage / 100 * (g_target - g_base))
+    b = int(b_base + percentage / 100 * (b_target - b_base))
+
+    curses.init_color(1, int(r / 0.255), int(g / 0.255), int(b / 0.255))
 
 
 def main_screen():
@@ -92,7 +150,9 @@ def main_screen():
                     "playerctl metadata artist", shell=True, stdout=subprocess.PIPE
                 )
                 length = subprocess.run(
-                    'playerctl metadata length --format "{{ duration(mpris:length) }}"', shell=True, stdout=subprocess.PIPE
+                    'playerctl metadata length --format "{{ duration(mpris:length) }}"',
+                    shell=True,
+                    stdout=subprocess.PIPE,
                 )
 
                 old_title_scrolled = title.stdout[:-1].decode("utf-8") + " "
@@ -117,32 +177,16 @@ def main_screen():
             box1.addstr(2, 2, " " * (half_cols - 2), curses.A_NORMAL)
             box1.addstr(3, 2, " " * (half_cols - 2), curses.A_NORMAL)
 
-            box1.addnstr(1, 2, "Title:  ", half_cols - 10, curses.color_pair(218))
+            box1.addnstr(1, 2, "Title:  ", half_cols - 10, curses.color_pair(2))
             box1.addnstr(1, 10, old_title_scrolled, half_cols - 11)
 
-            box1.addnstr(2, 2, "Album:  ", half_cols - 10, curses.color_pair(118))
+            box1.addnstr(2, 2, "Album:  ", half_cols - 10, curses.color_pair(2))
             box1.addnstr(2, 10, old_album_scrolled, half_cols - 11)
 
-            box1.addnstr(3, 2, "Artist: ", half_cols - 10, curses.color_pair(220))
+            box1.addnstr(3, 2, "Artist: ", half_cols - 10, curses.color_pair(2))
             box1.addnstr(3, 10, old_artist_scrolled, half_cols - 11)
 
-
-            
-            # progress bar
-            pos_str = position.stdout[:-1].decode("utf-8")
-            cur_pos = sum(int(x) * 60 ** i for i, x in enumerate(pos_str.split(":")[::-1]))
-
-            length_str = length.stdout[:-1].decode("utf-8")
-            cur_length = sum(int(x) * 60 ** i for i, x in enumerate(length_str.split(":")[::-1]))
-
-            percentage = cur_pos / cur_length * 100
-            percentage = int(percentage)
-            progress_bar = (half_cols - 4) * "-"
-            progress_bar = progress_bar[:int(percentage / 100 * (half_cols - 4))] + "⬤" + progress_bar[int(percentage / 100 * (half_cols - 4)) + 1:]
-            box1.addnstr(6, 2, progress_bar, half_cols - 4)
-
-            box1.addstr(5, 2, position_str, curses.color_pair(get_color_from_percentage(215, percentage)))
-            box1.addstr(5, half_cols - 2 - len(length_str), length_str, curses.color_pair(131))
+            draw_progress_bar(box1, position_str, length_str, half_cols)
 
             screen.refresh()
             screen.getch()
@@ -152,6 +196,3 @@ def main_screen():
 
 
 main_screen()
-
-
-
